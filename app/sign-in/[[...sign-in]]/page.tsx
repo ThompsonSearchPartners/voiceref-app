085ffd0979ddbea977d230259b396c4a5d9e57d6
ThompsonSearchPartners <conor@thompsonsearchpartners.com>
2025-11-19 20:20:39 -0500

    Create page.tsx

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,19 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
+      <SignIn 
+        appearance={{
+          elements: {
+            rootBox: "mx-auto",
+            card: "shadow-xl"
+          }
+        }}
+        routing="path"
+        path="/sign-in"
+        signUpUrl="/sign-up"
+      />
+    </div>
+  )
+}
