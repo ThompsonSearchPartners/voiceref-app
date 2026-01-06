73b0768dd748cce4ba1cfa7cef2064748ac68c03
ThompsonSearchPartners <conor@thompsonsearchpartners.com>
2026-01-06 16:01:26 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -15,9 +15,9 @@ const isPublicRoute = createRouteMatcher([
   '/api/cron(.*)',
 ])
 
-export default clerkMiddleware(async (auth, request) => {
+export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect()
+    auth().protect()
   }
 })
 
